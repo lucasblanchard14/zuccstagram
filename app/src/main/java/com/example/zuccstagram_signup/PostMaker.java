8a5b1f3eef9214b363431adb59369a5d843a83ec
iggcomp <60203945+iggcomp@users.noreply.github.com>
2020-02-13 01:07:14 -0500

    java file for posting pictures
    contains buttons for accessing camera/gallery for purpose of uploading to the database. database partially complete

---
+++ app/src/main/java/com/example/zuccstagram_signup/PostMaker.java
@@ -0,0 +1,125 @@
+package com.example.zuccstagram_post;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PostMaker extends AppCompatActivity {
+    private final static int REQUEST_IMAGE_CAPTURE = 1;
+    private final static int PICK_IMAGE = 1;
+    Uri imageUri;
+    private ImageView imageView;
+    EditText description;
+    Button galleryButton;
+    Button cameraButton;
+    Button uploadButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_maker);
+
+        setUpUI();
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dispatchGalleryPicture();
+            }
+        });
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dispatchCameraPicture();
+            }
+        });
+    }
+
+    private void dispatchCameraPicture() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePicture.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePicture, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    protected void dispatchGalleryPicture() {
+        Intent gallery = new Intent();
+        gallery.setType("image/*");
+        gallery.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(gallery, "Select picture"), PICK_IMAGE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    void setUpUI() {
+        description = findViewById(R.id.description);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        cameraButton = findViewById(R.id.cameraButton);
+        galleryButton = findViewById(R.id.galleryButton);
+        uploadButton = findViewById(R.id.uploadButton);
+
+    }
+
+    void uploadPost() {
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("Description", description);
+        docData.put("Image", "TBD, filename goes here");
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("Posts").document("test")
+                .set(docData)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //Log.d(TAG, "DocumentSnapshot successfully written!");
+                        // SUCCESS
+                        // uhh do something here like return to main page
+                        Log.d("PostMaker", "Account Created.");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //Log.w(TAG, "Error writing document", e);
+                        // ERROR HANDLER
+                    }
+                });
+    }
+}
