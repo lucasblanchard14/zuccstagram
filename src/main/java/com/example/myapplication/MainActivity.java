5f730f505bf2518cbd90003689e737e87990678a
lucasblanchard14 <55551979+lucasblanchard14@users.noreply.github.com>
2020-02-27 20:27:20 -0500

    Added project containing redesigned UI
    Profile, notifications, options dropdown, new bottom appbar. Top appbar is consitent across fragments.

---
+++ src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.textView19:
+                //TODO
+                return true;
+
+            case R.id.textView20:
+                //TODO
+                return true;
+
+            case R.id.textView21:
+                //TODO
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+
+        /*AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();*/
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        NavigationUI.setupWithNavController(navView, navController);
+
+
+    }
+
+}
